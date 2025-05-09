cfb4cfa715a8bc4555960df94ba98dde123f82aa
Awu Chen <awu.mxr@gmail.com>
2025-05-09 15:16:40 -0700

    Updates

--- precache-manifest.5e97649511074cae1b94300706a9c3b7.js
+++ precache-manifest.5e97649511074cae1b94300706a9c3b7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1df433bf43a6f49bceb486998bc48887",
+    "revision": "a352307524875b8754f93a910ce2171a",
     "url": "/craft-network/index.html"
   },
   {
-    "revision": "fc6c7a9da25944e537a1",
+    "revision": "efc416d4bdf04fa4ff7a",
     "url": "/craft-network/static/css/main.01b6fc35.chunk.css"
   },
   {
-    "revision": "ca45f31a24d9bf3cb234",
-    "url": "/craft-network/static/js/2.20643f41.chunk.js"
+    "revision": "a475cf990438d5fabf9c",
+    "url": "/craft-network/static/js/2.45af68eb.chunk.js"
   },
   {
     "revision": "7b36c4a4b00d4c1eb1f21a15ccb34795",
-    "url": "/craft-network/static/js/2.20643f41.chunk.js.LICENSE.txt"
+    "url": "/craft-network/static/js/2.45af68eb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fc6c7a9da25944e537a1",
-    "url": "/craft-network/static/js/main.7482e601.chunk.js"
+    "revision": "efc416d4bdf04fa4ff7a",
+    "url": "/craft-network/static/js/main.cbc50f36.chunk.js"
   },
   {
     "revision": "ba327305ee31a548d2fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/craft-network/precache-manifest.7a2eb817ab7c88cfb55b03133edcf2fa.js"
+  "/craft-network/precache-manifest.5e97649511074cae1b94300706a9c3b7.js"
 );
 
 self.addEventListener('message', (event) => {
