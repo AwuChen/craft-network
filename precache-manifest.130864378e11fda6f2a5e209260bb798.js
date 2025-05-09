4523ad7ffc0596826ab4e8b9703bce7106006f1f
Awu Chen <awu.mxr@gmail.com>
2025-05-09 14:53:30 -0700

    Updates

--- precache-manifest.130864378e11fda6f2a5e209260bb798.js
+++ precache-manifest.130864378e11fda6f2a5e209260bb798.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "63f94f4f57270998a38c56616e8c89d4",
+    "revision": "ed2cb86fcf6cb8b8893670f444bdc5cc",
     "url": "/craft-network/index.html"
   },
   {
-    "revision": "19157cad20afdb4d1b42",
+    "revision": "31f5431f7631fc522844",
     "url": "/craft-network/static/css/main.01b6fc35.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/craft-network/static/js/2.20643f41.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "19157cad20afdb4d1b42",
-    "url": "/craft-network/static/js/main.757baf7f.chunk.js"
+    "revision": "31f5431f7631fc522844",
+    "url": "/craft-network/static/js/main.80c3b1ab.chunk.js"
   },
   {
     "revision": "ba327305ee31a548d2fa",
