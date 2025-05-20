7272cd48a518c1c8f7336f21028a169988b375e5
Awu Chen <awu.mxr@gmail.com>
2025-05-20 05:53:07 -0700

    fixed compiler error missing import

--- react-graph-viz/src/CypherViz.js
+++ react-graph-viz/src/CypherViz.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation, useParams } from 'react-router-dom';
 import './App.css';
 import ForceGraph2D from 'react-force-graph-2d';
 
